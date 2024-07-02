bac716847d88b430a08682052d9437b56d523d43
Kasper Ragnar Fjelrad <kja@unik.dk>
2024-07-02 15:58:34 +0200

    Finished exercise 1,2,3

--- src/main/java/com/example/DatabaseConfig.java
+++ src/main/java/com/example/DatabaseConfig.java
@@ -10,11 +10,21 @@ import java.sql.SQLException;
 
 @Configuration
 public class DatabaseConfig {
+
+    private String user = System.getenv("DB_USER");
+    private String password = System.getenv("DB_PWD");
+    private String serverName = System.getenv("DB_SERVER"); // db host name, like localhost without the port
+    private String dbName = System.getenv("DB_NAME");
     private MysqlDataSource datasource = new MysqlDataSource();
 
     public DatabaseConfig() {
-                datasource.setPort(3306); //default config
-        datasource.setURL("jdbc:mysql://localhost:3306/mysqldb");
+        datasource.setPassword(password);
+        datasource.setUser(user);
+        datasource.setServerName(serverName);
+        datasource.setDatabaseName(dbName);
+
+        //datasource.setPort(3306); //default config
+        //datasource.setURL("jdbc:mysql://localhost:3306/dbname");
     }
 
     @Bean
